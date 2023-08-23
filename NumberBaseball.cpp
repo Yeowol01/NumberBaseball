27c5f7d2469568d9bddc4862b59a78687c079674
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-08-23 19:47:47 +0900

    (2023.08.23) C언어로 게임 만들기 2일차

--- NumberBaseball.cpp
+++ NumberBaseball.cpp
@@ -45,5 +45,71 @@ int main()
 		Count--;
 	}
 
+	while (1)
+	{
+		int Strike = 0;
+		int Ball = 0;
+		int i, j;
+
+		system("cls");
+
+		for (i = 0; i < UserInputCount; i++)
+		{
+			printf("%d %d %d\n", UserInputTable[i][0], UserInputTable[i][1], UserInputTable[i][2]);
+		}
+
+		printf("0 ~ 9 까지 숫자를 3개 입력해 주세요 ex) 1 2 3\n");
+		printf("종료는 -1을 눌러주세요.\n");
+		printf("기회는 20번. 남은기회 : %d번\n", 20 - UserInputCount);
+		printf("입력 : ");
+
+		for (i = 0; i < 3; i++)
+		{
+			scanf_s("%d", &User[i]);
+			if (User[i] == -1)
+			break;
+		}
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (Computer[i] == User[j])
+				{
+					if (i == j)
+						Strike++;
+					else
+						Ball++;
+				}
+			}
+		}
+
+		if (Strike == 3)
+		{
+			printf("아웃! 게임을 종료합니다.\n");
+			break;
+		}
+		else
+		{
+			printf("%s %s\n", StrikeString[Strike], BallString[Ball]);
+		}
+
+		UserInputTable[UserInputCount][0] = User[0];
+		UserInputTable[UserInputCount][1] = User[1];
+		UserInputTable[UserInputCount][2] = User[2];
+
+		UserInputCount++;
+
+		if (UserInputCount >= 20)
+		{
+			printf("모든 기회를 소진하셨습니다.\n");
+			printf("정답 : %d %d %d\n", Computer[0], Computer[1], Computer[2]);
+			break;
+		}
+
+		printf("....Press Any Key....");
+		_getch();
+	}
+
 	return 0;
 }
